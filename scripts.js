76b7cc6fd5b27ec274ce92e37aa52685fca5fd87
jared282 <jared282@gmail.com>
2023-02-17 19:19:05 -0700

    scoreboard buttons work

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 "use strict";
 
-// MODAL FUNCTIONALITY
-const modal = document.getElementById("myModal");
-const btn = document.getElementById("myBtn");
-const span = document.getElementsByClassName("close")[0];
+const modal = document.querySelector("#myModal");
+const btn = document.querySelector("#myBtn");
+const span = document.querySelector(".close");
+let totalScoreBlue = 0;
+let totalScoreRed = 0;
 
+// MODAL FUNCTIONALITY
 btn.onclick = function () {
   modal.style.display = "block";
 };
@@ -21,4 +23,79 @@ document.addEventListener("keydown", function (event) {
   if (event.key === "Escape") {
     modal.style.display = "none";
   }
+});
+
+// SCOREBOARD FUNCTIONALITY
+// BLUE SCORE
+document.querySelector(".score-blue").textContent = totalScoreBlue;
+document
+  .querySelector(".body-regular-kick-blue")
+  .addEventListener("click", function () {
+    totalScoreBlue = totalScoreBlue + 2;
+    document.querySelector(".score-blue").textContent = totalScoreBlue;
+  });
+document
+  .querySelector(".body-turning-kick-blue")
+  .addEventListener("click", function () {
+    totalScoreBlue = totalScoreBlue + 4;
+    document.querySelector(".score-blue").textContent = totalScoreBlue;
+  });
+document
+  .querySelector(".head-regular-kick-blue")
+  .addEventListener("click", function () {
+    totalScoreBlue = totalScoreBlue + 3;
+    document.querySelector(".score-blue").textContent = totalScoreBlue;
+  });
+document
+  .querySelector(".head-turning-kick-blue")
+  .addEventListener("click", function () {
+    totalScoreBlue = totalScoreBlue + 5;
+    document.querySelector(".score-blue").textContent = totalScoreBlue;
+  });
+document
+  .querySelector(".technical-point-blue")
+  .addEventListener("click", function () {
+    totalScoreBlue = totalScoreBlue + 1;
+    document.querySelector(".score-blue").textContent = totalScoreBlue;
+  });
+document.querySelector(".penalty-blue").addEventListener("click", function () {
+  totalScoreRed = totalScoreRed + 1;
+  document.querySelector(".score-red").textContent = totalScoreRed;
+});
+
+// RED SCORE
+document.querySelector(".score-red").textContent = totalScoreRed;
+document
+  .querySelector(".body-regular-kick-red")
+  .addEventListener("click", function () {
+    totalScoreRed = totalScoreRed + 2;
+    document.querySelector(".score-red").textContent = totalScoreRed;
+  });
+document
+  .querySelector(".body-turning-kick-red")
+  .addEventListener("click", function () {
+    totalScoreRed = totalScoreRed + 4;
+    document.querySelector(".score-red").textContent = totalScoreRed;
+  });
+document
+  .querySelector(".head-regular-kick-red")
+  .addEventListener("click", function () {
+    totalScoreRed = totalScoreRed + 3;
+    document.querySelector(".score-red").textContent = totalScoreRed;
+  });
+document
+  .querySelector(".head-turning-kick-red")
+  .addEventListener("click", function () {
+    totalScoreRed = totalScoreRed + 5;
+    document.querySelector(".score-red").textContent = totalScoreRed;
+  });
+document
+  .querySelector(".technical-point-red")
+  .addEventListener("click", function () {
+    totalScoreRed = totalScoreRed + 1;
+    document.querySelector(".score-red").textContent = totalScoreRed;
+  });
+document.querySelector(".penalty-red").addEventListener("click", function () {
+  totalScoreBlue = totalScoreBlue + 1;
+  document.querySelector(".score-blue").textContent = totalScoreBlue;
 });
