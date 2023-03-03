42d334a61163294b00ed88c60751d2b468fc84f7
jared282 <jared282@gmail.com>
2023-03-02 23:48:21 -0700

    refactored my code to store things in functions

--- scripts.js
+++ scripts.js
@@ -12,26 +12,21 @@ let totalScoreRed = 0;
 let interval;
 var audioBuzzer = document.getElementById("myAudio");
 var audioRumble = document.getElementById("myAudioRumble");
-let roundNumbers = 1; // To get this to cycle through, I should make a for loop. for(let i = 1; i <= 3; i++) and create a timer function that includes the stop, start, and rounds functionality so that i loops through.
+var timerBreak;
+var timerStart;
+var minutes;
+var seconds;
+let roundNumbers = 1;
 
-// FUNCTIONS AUDIO SOUNDS
+// FUNCTIONS
 
-function playAudio() {
-  audioBuzzer.play();
-}
-
-function playAudioRumble() {
-  audioRumble.play();
-}
-
-// MODAL FUNCTIONALITY
+// Settings Modal
 btn.onclick = function () {
   modal.style.display = "block";
 };
 span.onclick = function () {
   modal.style.display = "none";
 };
-
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
@@ -43,82 +38,63 @@ document.addEventListener("keydown", function (event) {
   }
 });
 
-// SCOREBOARD FUNCTIONALITY
-// BLUE SCORE
-document.querySelector(".score-blue").textContent = totalScoreBlue;
-document
-  .querySelector(".technical-point-blue")
-  .addEventListener("click", function () {
-    totalScoreBlue = totalScoreBlue + 1;
-    document.querySelector(".score-blue").textContent = totalScoreBlue;
-  });
-document.querySelector(".penalty-blue").addEventListener("click", function () {
-  totalScoreBlue = totalScoreBlue - 1;
-  document.querySelector(".score-blue").textContent = totalScoreBlue;
-});
+// Audio Sounds
+function playAudio() {
+  audioBuzzer.play();
+}
+function playAudioRumble() {
+  audioRumble.play();
+}
 
-// RED SCORE
-document.querySelector(".score-red").textContent = totalScoreRed;
-document
-  .querySelector(".technical-point-red")
-  .addEventListener("click", function () {
-    totalScoreRed = totalScoreRed + 1;
+// Scoring buttons for red and blue
+function blueScore() {
+  document.querySelector(".score-blue").textContent = totalScoreBlue;
+  document
+    .querySelector(".technical-point-blue")
+    .addEventListener("click", function () {
+      totalScoreBlue = totalScoreBlue + 1;
+      document.querySelector(".score-blue").textContent = totalScoreBlue;
+    });
+  document
+    .querySelector(".penalty-blue")
+    .addEventListener("click", function () {
+      totalScoreBlue = totalScoreBlue - 1;
+      document.querySelector(".score-blue").textContent = totalScoreBlue;
+    });
+}
+blueScore();
+
+function redScore() {
+  document.querySelector(".score-red").textContent = totalScoreRed;
+  document
+    .querySelector(".technical-point-red")
+    .addEventListener("click", function () {
+      totalScoreRed = totalScoreRed + 1;
+      document.querySelector(".score-red").textContent = totalScoreRed;
+    });
+  document.querySelector(".penalty-red").addEventListener("click", function () {
+    totalScoreRed = totalScoreRed - 1;
     document.querySelector(".score-red").textContent = totalScoreRed;
   });
-document.querySelector(".penalty-red").addEventListener("click", function () {
-  totalScoreRed = totalScoreRed - 1;
-  document.querySelector(".score-red").textContent = totalScoreRed;
-});
-
-// NAME OF THE FIGHTER
-document
-  .querySelector("#blue-name-input")
-  .addEventListener("click", function () {
-    const blueFighter = prompt("What is the name of the blue fighter?");
-    if (blueFighter === null) {
-      document.querySelector(".blue-name").textContent = "Blue";
-    } else {
-      document.querySelector(".blue-name").textContent = blueFighter;
-    }
-  });
-document
-  .querySelector("#red-name-input")
-  .addEventListener("click", function () {
-    const redFighter = prompt("What is the name of the red fighter?");
-    if (redFighter === null) {
-      document.querySelector(".red-name").textContent = "Red";
-    } else {
-      document.querySelector(".red-name").textContent = redFighter;
-    }
-  });
-
-// START AND STOP THE TIME AND COLORS
+}
+redScore();
 
 function countdownBreak() {
   clearInterval(interval);
   interval = setInterval(function () {
-    var timer = $(".time-clock-break").html();
-    timer = timer.split(":");
-    var minutes = timer[0];
-    var seconds = timer[1];
+    timerBreak = $(".time-clock-break").html();
+    timerBreak = timerBreak.split(":");
+    minutes = timerBreak[0];
+    seconds = timerBreak[1];
     seconds -= 1;
     if (minutes < 0) return;
     else if (seconds < 0 && minutes != 0) {
       minutes -= 0;
       seconds = 2;
     } else if (seconds < 10 && seconds.length != 2) seconds = "0" + seconds;
-
     $(".time-clock-break").html(minutes + ":" + seconds);
-
     if (minutes == 0 && seconds == 0) {
       clearInterval(interval);
-
-      while (roundNumbers <= 3) {
-        roundNumbers++;
-        break;
-      }
-      console.log(roundNumbers);
-      document.querySelector(".round-number").textContent = roundNumbers;
       document.querySelector(".time-clock").textContent = "0:03";
       document.querySelector("#timer").style.color = "#FFFFFF";
     }
@@ -127,23 +103,23 @@ function countdownBreak() {
     }
   }, 1000);
 }
+countdownBreak();
 
-function countdown() {
+// Starts the timer
+function startTimer() {
   clearInterval(interval);
   interval = setInterval(function () {
-    var timer = $(".time-clock").html();
-    timer = timer.split(":");
-    var minutes = timer[0];
-    var seconds = timer[1];
+    timerStart = $(".time-clock").html();
+    timerStart = timerStart.split(":");
+    minutes = timerStart[0];
+    seconds = timerStart[1];
     seconds -= 1;
     if (minutes < 0) return;
     else if (seconds < 0 && minutes != 0) {
       minutes -= 0;
       seconds = 3;
     } else if (seconds < 10 && seconds.length != 2) seconds = "0" + seconds;
-
     $(".time-clock").html(minutes + ":" + seconds);
-
     if (minutes == 0 && seconds == 0) {
       clearInterval(interval);
       document.querySelector("#timer").style.color = "#FF0000";
@@ -152,41 +128,99 @@ function countdown() {
     }
   }, 1000);
 }
+startTimer();
 
+// Stops the timer
 function stopCounter() {
   clearInterval(interval);
 }
+stopCounter();
 
-document.querySelector("#startTime").addEventListener("click", countdown);
+// Button to stop the timer
+function buttonStopTimer() {
+  clearInterval(interval);
+  document.querySelector("#stopTime").addEventListener("click", stopCounter);
+}
+buttonStopTimer();
 
-document.querySelector("#stopTime").addEventListener("click", stopCounter);
+// Button to start the timer
+function buttonStartTimer() {
+  document.querySelector("#startTime").addEventListener("click", startTimer);
+}
+buttonStartTimer();
 
-document.querySelector("#adjust-time").addEventListener("click", function () {
-  let adjustedTimer = prompt("What is the adjusted time?", "0:00");
-  adjustedTimer = adjustedTimer;
-  document.querySelector("#timer").textContent = adjustedTimer;
-  if (adjustedTimer === null || adjustedTimer === "0:00") {
-    alert("Please input an adjusted time");
-    prompt("What is the adjusted time?", "0:00");
-  }
-});
+// Change round numbers
+function roundNumberChange() {
+  roundNumbers++;
+  document.querySelector(".round-number").textContent = roundNumbers;
+}
 
-// ROUNDS
-document
-  .querySelector("#change-round-number")
-  .addEventListener("click", function () {
-    let roundNumber = prompt("Enter a number between 1 and 3.");
-    roundNumber = Number(roundNumber);
+// Change rounds in the modal
+function changeRoundNumber() {
+  document
+    .querySelector("#change-round-number")
+    .addEventListener("click", function () {
+      let roundNumber = prompt("Enter a number between 1 and 3.");
+      roundNumber = Number(roundNumber);
 
-    if (roundNumber === null) {
-      document.querySelector(".round-number").textContent = "1";
-    } else if (isNaN(roundNumber)) {
-      alert(
-        "Your entry is not a number. Please enter a number between 1 and 3."
-      );
-    } else if (roundNumber >= 4) {
-      alert("Please enter a number between 1 and 3.");
-    } else {
-      document.querySelector(".round-number").textContent = roundNumber;
+      if (roundNumber === null) {
+        document.querySelector(".round-number").textContent = "1";
+      } else if (isNaN(roundNumber)) {
+        alert(
+          "Your entry is not a number. Please enter a number between 1 and 3."
+        );
+      } else if (roundNumber >= 4) {
+        alert("Please enter a number between 1 and 3.");
+      } else {
+        document.querySelector(".round-number").textContent = roundNumber;
+      }
+    });
+}
+changeRoundNumber();
+
+// Change the time in the modal
+function changeTime() {
+  document.querySelector("#adjust-time").addEventListener("click", function () {
+    let adjustedTimer = prompt("What is the adjusted time?", "0:00");
+    adjustedTimer = adjustedTimer;
+    document.querySelector("#timer").textContent = adjustedTimer;
+    if (adjustedTimer === null || adjustedTimer === "0:00") {
+      alert("Please input an adjusted time");
+      prompt("What is the adjusted time?", "0:00");
     }
   });
+}
+changeTime();
+
+// Change the name of the blue fighter
+function changeBlueFighter() {
+  document
+    .querySelector("#blue-name-input")
+    .addEventListener("click", function () {
+      const blueFighter = prompt("What is the name of the blue fighter?");
+      if (blueFighter === null) {
+        document.querySelector(".blue-name").textContent = "Blue";
+      } else {
+        document.querySelector(".blue-name").textContent = blueFighter;
+      }
+    });
+}
+changeBlueFighter();
+
+// Change the name of the red fighter
+function changeRedFighter() {
+  document
+    .querySelector("#red-name-input")
+    .addEventListener("click", function () {
+      const redFighter = prompt("What is the name of the red fighter?");
+      if (redFighter === null) {
+        document.querySelector(".red-name").textContent = "Red";
+      } else {
+        document.querySelector(".red-name").textContent = redFighter;
+      }
+    });
+}
+changeRedFighter();
+
+// END FUNCTIONS
+// ------------------------------------------------------------------
